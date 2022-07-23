51dad92ff04c2e98cb567a17c092ea2b9d915c16
Arthur Tavares <arthurtquintao@gmail.com>
2022-07-23 09:56:42 -0300

    add LightBulb class

---
+++ 2 - dependency inversion/tests/LightBulb.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+from SwitchableInterface import Switchable
+
+
+class LightBulb(Switchable):
+    def turn_on(self):
+        print("LightBulb: turned on...")
+
+    def turn_off(self):
+        print("LightBulb: turned off...")
