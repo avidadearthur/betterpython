5d86559d9b6cec2078fa301bd8935d514ba507ee
Arthur Tavares <arthurtquintao@gmail.com>
2022-07-23 10:06:04 -0300

    add power switch class

---
+++ 2 - dependency inversion/tests/ElectricPowerSwitch.py
@@ -0,0 +1,17 @@
+import imp
+from SwitchableInterface import Switchable
+
+
+class ElectricPowerSwitch:
+
+    def __init__(self, c: Switchable):
+        self.client = c
+        self.on = False
+
+    def press(self):
+        if self.on:
+            self.client.turn_off()
+            self.on = False
+        else:
+            self.client.turn_on()
+            self.on = True
