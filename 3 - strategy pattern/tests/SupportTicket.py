0b3a0059a5542120e3dfb4343a61a79656ee0e2b
Arthur Tavares <arthurtquintao@gmail.com>
2022-08-11 09:55:25 -0300

    Create support ticket class with random id gen

---
+++ 3 - strategy pattern/tests/SupportTicket.py
@@ -0,0 +1,17 @@
+import string
+from typing import List
+from abc import ABC, abstractmethod
+from random import random, choices
+
+
+def generate_id(length=8):
+    # helper function for generating an id
+    return ''.join(random.choices(string.ascii_uppercase, k=length))
+
+
+class SupportTicket:
+
+    def __init__(self, customer, issue):
+        self.id = generate_id()
+        self.customer = customer
+        self.issue = issue
